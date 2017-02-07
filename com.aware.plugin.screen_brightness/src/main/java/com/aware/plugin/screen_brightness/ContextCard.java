56e57f6af2eb6e786bc221f84d0f76c30c260dfe
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2017-02-07 04:43:34 +0200

    Make the card a bit prettier

--- com.aware.plugin.screen_brightness/src/main/java/com/aware/plugin/screen_brightness/ContextCard.java
+++ com.aware.plugin.screen_brightness/src/main/java/com/aware/plugin/screen_brightness/ContextCard.java
@@ -1,19 +1,25 @@
 package com.aware.plugin.screen_brightness;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.icu.text.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.aware.utils.IContextCard;
 
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
 public class ContextCard implements IContextCard {
     private BrightnessListener brightnessListener = new BrightnessListener();
-    private TextView text;
+    private TextView brightness, autoBrightness, lastUpdated;
     public class BrightnessListener extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -26,23 +32,27 @@ public class ContextCard implements IContextCard {
         // Not implemented
     }
 
+    @SuppressLint("SetTextI18n")
     private void updateBrightness(Context context){
         Cursor latest = context.getContentResolver().query(Provider.Brightness_Data.CONTENT_URI, null, null, null, Provider.Brightness_Data.TIMESTAMP + " DESC LIMIT 1");
         if(latest != null && latest.moveToFirst()){
             String brightness = latest.getString(latest.getColumnIndex(Provider.Brightness_Data.BRIGHTNESS));
             String autoBrightness = latest.getString(latest.getColumnIndex(Provider.Brightness_Data.AUTO_BRIGHTNESS));
-            text.setText("Brightness: " + brightness + " Auto-brightness: " + autoBrightness);
+            this.brightness.setText("Brightness: " + brightness);
+            this.autoBrightness.setText("Auto-brightness: " + autoBrightness);
         }
         if(latest != null && !latest.isClosed()){
             latest.close();
         }
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View getContextCard(Context context) {
         View card = LayoutInflater.from(context).inflate(R.layout.card, null);
-        text = (TextView) card.findViewById(R.id.hello);
-        text.setText("Loading..");
+        brightness = (TextView) card.findViewById(R.id.brightness);
+        autoBrightness = (TextView) card.findViewById(R.id.autoBrightness);
+        brightness.setText("Loading..");
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Plugin.ACTION_PLUGIN_SCREEN_BRIGHTNESS);
